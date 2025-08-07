e3447ebe4dfae7ab38b244b4dbb9cf561c329ecc
Raheem Orekoya <tundeorekoya1@gmail.com>
2025-08-07 07:43:34 +0100

    fix:made bolt version available in prod

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router";
-import BoltVersion from "@/pages/bolt.dev";
+import BoltVersion from "@/pages/bolt";
 import LandingPage from "@/pages/landing-page";
 import SignIn from "@/pages/sign-in";
 import SignUp from "@/pages/sign-up";
@@ -11,8 +11,7 @@ export default function App() {
          <Route element={<Layout />}>
             <Route path="/" element={<LandingPage />} />
             <Route path="/bolt-version" element={<BoltVersion />} />
-            
-            
+
             {/* Auth Routes */}
             <Route path="/auth/sign-in" element={<SignIn />} />
             <Route path="/auth/sign-up" element={<SignUp />} />
