4a9f1d289ec0a3530b9b4d3cb3fef2066f01dfc2
Raheem Orekoya <tundeorekoya1@gmail.com>
2025-08-07 20:01:33 +0100

    fix: fix import meta

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-   baseURL: process.env.VITE_SERVER_URL,
+   baseURL: import.meta.env.VITE_SERVER_URL,
 });
