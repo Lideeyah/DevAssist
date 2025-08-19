d95e0b4b902783685c100ddaf4463ab3de0f188a
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-08-19 11:34:09 -0700

    feat: implement tab switching between code and preview modes in CodePrompt component

--- src/pages/dashboard/layout.tsx
+++ src/pages/dashboard/layout.tsx
@@ -13,8 +13,8 @@ export default function DashboardLayout(): JSX.Element {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex fixed w-full z-[9999] bg-background justify-between pr-4 overflow-hidden border-b h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
+        <header className="flex fixed w-[81%] top-0 z-[9999] bg-background justify-between pr-4 overflow-hidden border-b h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4 w-full">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
           </div>
@@ -28,21 +28,21 @@ export default function DashboardLayout(): JSX.Element {
             </div>
           ) : (
             <div className="flex items-center justify-end w-full gap-4">
-              <div className="bg-neutral-700 rounded-lg p-2 hover:scale-95 transition-all duration-200 cursor-pointer">
+              <div className="bg-neutral-700 rounded-sm p-2 hover:scale-95 transition-all duration-200 cursor-pointer">
                 <Settings size={17} />
               </div>
               <div className="hover:scale-95 transition-all duration-200 cursor-pointer">
                 <FaGithub size={20} />
               </div>
-              <div className="border rounded-lg p-2 hover:scale-95 transition-all duration-200 cursor-pointer">
+              <div className="border rounded-sm p-2 hover:scale-95 transition-all duration-200 cursor-pointer">
                 <FaBolt className="text-green-600" size={17} />
               </div>
-              <div className="bg-neutral-800 hover:scale-95 transition-all duration-200 cursor-pointer p-2 rounded-lg flex items-center gap-3">
+              <div className="bg-neutral-800 hover:scale-95 transition-all duration-200 cursor-pointer p-2 rounded-sm flex items-center gap-3">
                 <LayoutGrid size={17} />
                 <span>Integration</span>
                 <ChevronDown size={17} />
               </div>
-              <div className="flex items-center hover:scale-95 transition-all duration-200 cursor-pointer bg-blue-500 rounded-lg p-2 gap-3">
+              <div className="flex items-center hover:scale-95 transition-all duration-200 cursor-pointer bg-blue-500 rounded-sm p-2 gap-3">
                 Publish
                 <ChevronDown size={17} />
               </div>
